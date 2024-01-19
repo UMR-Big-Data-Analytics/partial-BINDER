e104a07d90209bf150ad5031b11fa5b1fa293e3e
Jakob L. Müller <me@jakob-l-m.de>
2024-01-19 16:30:06 +0100

    minor renames

--- BINDERAlgorithm/src/binder/runner/MetanomeMock.java
+++ BINDERAlgorithm/src/binder/runner/MetanomeMock.java
@@ -3,14 +3,10 @@ package binder.runner;
 import binder.BINDERFile;
 import binder.core.BINDER;
 import binder.utils.FileUtils;
-import de.metanome.algorithm_integration.AlgorithmConfigurationException;
 import de.metanome.algorithm_integration.AlgorithmExecutionException;
 import de.metanome.algorithm_integration.ColumnIdentifier;
 import de.metanome.algorithm_integration.configuration.ConfigurationSettingFileInput;
 import de.metanome.algorithm_integration.input.FileInputGenerator;
-import de.metanome.algorithm_integration.input.InputGenerationException;
-import de.metanome.algorithm_integration.input.RelationalInput;
-import de.metanome.algorithm_integration.input.RelationalInputGenerator;
 import de.metanome.algorithm_integration.results.InclusionDependency;
 import de.metanome.algorithm_integration.results.Result;
 import de.metanome.backend.input.file.DefaultFileInputGenerator;
@@ -22,27 +18,13 @@ import java.util.*;
 
 public class MetanomeMock {
 
-    public static List<ColumnIdentifier> getAcceptedColumns(RelationalInputGenerator[] relationalInputGenerators) throws InputGenerationException, AlgorithmConfigurationException {
-        List<ColumnIdentifier> acceptedColumns = new ArrayList<>();
-        for (RelationalInputGenerator relationalInputGenerator : relationalInputGenerators) {
-            RelationalInput relationalInput = relationalInputGenerator.generateNewCopy();
-            String tableName = relationalInput.relationName();
-            for (String columnName : relationalInput.columnNames())
-                acceptedColumns.add(new ColumnIdentifier(tableName, columnName));
-        }
-        return acceptedColumns;
-    }
-
     public static void executeBINDER(Config conf) {
         try {
             BINDER binder;
             FileInputGenerator[] fileInputGenerators = new FileInputGenerator[conf.tableNames.length];
             ResultCache resultReceiver = new ResultCache("MetanomeMock", null);
             for (int i = 0; i < conf.tableNames.length; i++)
-                fileInputGenerators[i] = new DefaultFileInputGenerator(new ConfigurationSettingFileInput(
-                        conf.inputFolderPath + conf.databaseName + File.separator + conf.tableNames[i] + conf.inputFileEnding, true,
-                        conf.inputFileSeparator, conf.inputFileQuotechar, conf.inputFileEscape, conf.inputFileStrictQuotes,
-                        conf.inputFileIgnoreLeadingWhiteSpace, conf.inputFileSkipLines, conf.inputFileHasHeader, conf.inputFileSkipDifferingLines, conf.inputFileNullString));
+                fileInputGenerators[i] = new DefaultFileInputGenerator(new ConfigurationSettingFileInput(conf.inputFolderPath + conf.databaseName + File.separator + conf.tableNames[i] + conf.inputFileEnding, true, conf.inputFileSeparator, conf.inputFileQuoteChar, conf.inputFileEscape, conf.inputFileStrictQuotes, conf.inputFileIgnoreLeadingWhiteSpace, conf.inputFileSkipLines, conf.inputFileHasHeader, conf.inputFileSkipDifferingLines, conf.inputFileNullString));
 
             BINDERFile binderFile = new BINDERFile();
             binderFile.setRelationalInputConfigurationValue(BINDERFile.Identifier.INPUT_FILES.name(), fileInputGenerators);
@@ -59,16 +41,16 @@ public class MetanomeMock {
             time = System.currentTimeMillis() - time;
 
             if (conf.writeResults) {
-                FileUtils.writeToFile(binder + "\r\n\r\n" + "Runtime: " + time + "\r\n\r\n" + conf.toString(), conf.measurementsFolderPath + File.separator + conf.statisticsFileName);
+                FileUtils.writeToFile(binder + "\r\n\r\n" + "Runtime: " + time + "\r\n\r\n" + conf, conf.measurementsFolderPath + File.separator + conf.statisticsFileName);
                 FileUtils.writeToFile(format(resultReceiver.fetchNewResults()), conf.measurementsFolderPath + File.separator + conf.resultFileName);
             }
         } catch (AlgorithmExecutionException | IOException e) {
             e.printStackTrace();
         }
-	}
+    }
 
     private static String format(List<Result> results) {
-        HashMap<String, List<String>> ref2Deps = new HashMap<String, List<String>>();
+        HashMap<String, List<String>> ref2Deps = new HashMap<>();
 
         for (Result result : results) {
             InclusionDependency ind = (InclusionDependency) result;
@@ -77,10 +59,8 @@ public class MetanomeMock {
             Iterator<ColumnIdentifier> refIterator = ind.getReferenced().getColumnIdentifiers().iterator();
             while (refIterator.hasNext()) {
                 refBuilder.append(refIterator.next().toString());
-                if (refIterator.hasNext())
-                    refBuilder.append(",");
-                else
-                    refBuilder.append(")");
+                if (refIterator.hasNext()) refBuilder.append(",");
+                else refBuilder.append(")");
             }
             String ref = refBuilder.toString();
 
@@ -88,31 +68,26 @@ public class MetanomeMock {
             Iterator<ColumnIdentifier> depIterator = ind.getDependant().getColumnIdentifiers().iterator();
             while (depIterator.hasNext()) {
                 depBuilder.append(depIterator.next().toString());
-                if (depIterator.hasNext())
-                    depBuilder.append(",");
-                else
-                    depBuilder.append(")");
+                if (depIterator.hasNext()) depBuilder.append(",");
+                else depBuilder.append(")");
             }
             String dep = depBuilder.toString();
 
-            if (!ref2Deps.containsKey(ref))
-                ref2Deps.put(ref, new ArrayList<String>());
+            if (!ref2Deps.containsKey(ref)) ref2Deps.put(ref, new ArrayList<String>());
             ref2Deps.get(ref).add(dep);
         }
 
         StringBuilder builder = new StringBuilder();
-        ArrayList<String> referenced = new ArrayList<String>(ref2Deps.keySet());
+        ArrayList<String> referenced = new ArrayList<>(ref2Deps.keySet());
         Collections.sort(referenced);
         for (String ref : referenced) {
             List<String> dependants = ref2Deps.get(ref);
             Collections.sort(dependants);
 
-            if (!dependants.isEmpty())
-                builder.append(ref + " > ");
+            if (!dependants.isEmpty()) builder.append(ref).append(" > ");
             for (String dependant : dependants)
-                builder.append(dependant + "  ");
-            if (!dependants.isEmpty())
-                builder.append("\r\n");
+                builder.append(dependant).append("  ");
+            if (!dependants.isEmpty()) builder.append("\r\n");
         }
         return builder.toString();
     }
--- BINDERAlgorithm/src/binder/runner/MetanomeTestRunner.java
+++ BINDERAlgorithm/src/binder/runner/MetanomeTestRunner.java
@@ -21,7 +21,7 @@ public class MetanomeTestRunner {
     }
 
     public static void main(String[] args) {
-        MetanomeTestRunner.run(new Config(Config.Algorithm.BINDER, Config.Database.FILE, Config.Dataset.NCVOTER_STATEWIDE_SMALL, -1, -1), "test");
+        MetanomeTestRunner.run(new Config(Config.Algorithm.BINDER, Config.Dataset.KAGGLE), "test");
     }
 
 
