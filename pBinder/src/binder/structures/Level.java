7aa6a48e60a5766dad572a5ac6bca09a695d8632
Jakob L. Müller <me@jakob-l-m.de>
2024-02-21 14:20:37 +0100

    reduce class and converted to record

--- pBinder/src/binder/structures/Level.java
+++ pBinder/src/binder/structures/Level.java
@@ -1,32 +1,12 @@
 package binder.structures;
 
-public class Level implements Comparable<Level> {
-	
-	private final int number;
-	private final int emptyBuckets;
-	
-	public int getNumber() {
-		return number;
-	}
-
-	public int getEmptyBuckets() {
-		return emptyBuckets;
-	}
-
-	public Level(int number, int emptyBuckets) {
-		this.number = number;
-		this.emptyBuckets = emptyBuckets;
-	}
+public record Level(int number, int emptyBuckets) implements Comparable<Level> {
 
 	@Override
 	public int compareTo(Level other) {
 		if (this.number == other.number)
 			return 0;
-		if (this.emptyBuckets > other.emptyBuckets)
-			return 1;
-		if (this.emptyBuckets < other.emptyBuckets)
-			return -1;
-		return 0;
+		return Integer.compare(this.emptyBuckets, other.emptyBuckets);
 	}
 
 	@Override
@@ -36,9 +16,8 @@ public class Level implements Comparable<Level> {
 
 	@Override
 	public boolean equals(Object obj) {
-		if (!(obj instanceof Level))
+		if (!(obj instanceof Level other))
 			return false;
-		Level other = (Level) obj;
 		return (this.number == other.number) || (this.emptyBuckets == other.emptyBuckets);
 	}
 
